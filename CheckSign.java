8660de14e3d40e7575e6fe7366c71ea8d779905a
nivedhaasandy <47312721+nivedhaasandy@users.noreply.github.com>
2019-02-11 12:32:58 +0530

    Create CheckSign.java

---
+++ CheckSign.java
@@ -0,0 +1,17 @@
+#Nivedha
+#Ask
+class CheckSign{
+public static void main(String[] qwe){
+int a=1;
+if(a>0){
+System.out.println("positive");
+}
+else if(a==0){
+System.out.println("zero");
+}
+else{
+System.out.println("negative");
+System.out.println("output");
+}
+}
+}
