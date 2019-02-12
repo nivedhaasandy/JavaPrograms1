041833dea137abda3e6a1ae7a67d51098b677e75
nivedhaasandy <47312721+nivedhaasandy@users.noreply.github.com>
2019-02-12 08:54:15 +0530

    Create VowelConsonant.java

---
+++ VowelConsonant.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class VowelConsonant{
+public static void main(String[] qwe){
+Scanner sscan=new Scanner(System.in);
+System.out.println("enter a char");
+char ch=scan.next().charAt(0);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+System.out.println("Vowel");
+else
+System.out.println("Consonant");
+}
+}
